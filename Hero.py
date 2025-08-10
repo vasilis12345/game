f1b4a4596a1f406aea15f2843918b3030ef2bfa1
vasilis <009vasana@gmail.com>
2025-08-10 18:46:55 +0300

    introduce config.json restart enemy file

--- Hero.py
+++ Hero.py
@@ -1,5 +1,6 @@
-from enemies import Enemies
-Enemies = Enemies()
+from enemies import Enemy
+import random
+Enemy = Enemy(random.randint( 1 , 10))
 class Hero :
     def __init__(self , health , attack , xp , level , level_req ) :
         self.health = 100
@@ -15,8 +16,8 @@ class Hero :
             self.attack += 3
             self.xp = self.xp - self.level_req
 
-    def get_attacked(self, Enemies ) :
-        self.health -= Enemies.attack
-    def do_attack(self , Enemies ):
-        Enemies.health -= self.attack
+    def get_attacked(self, Enemy ) :
+        self.health -= Enemy.attack
+    def do_attack(self , Enemy):
+        Enemy.health -= self.attack
 
