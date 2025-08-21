3789cea1915ba3b6d157aa28cbf7b26d915d0faf
vasilis <009vasana@gmail.com>
2025-08-21 19:45:26 +0300

    new stuff fixed

--- Hero.py
+++ Hero.py
@@ -4,25 +4,30 @@ enemy = Enemy()
 
 
 class Hero :
-    def __init__(self , attack , health , level , xp_req , xp_held , critical_chance) :
+    def __init__(self , attack , health , level , xp_req , xp_held , critical_chance , critical_damage_multiplier) :
         self.attack = 25
         self.health = 100
         self.xp_req = 1000
         self.xp_held = 0
         self.level = 1
         self.critical_chance = random.randint(1 , 100)
+        self.critical_damage_multiplier = 1.9
     def level_up(self) :
         if  self.xp_req >= self.xp_held :
             self.level += 1
             self.attack += 5
             self.health += 25
             self.critical_chance = random.randint(1 , self.level - 101)
+            self.critical_damage_multiplier += 0.3
             self.xp_held -= self.xp_req
             self.xp_req += 250
     def do_attack(self) :
-
-        enemy.enemy_health -= self.attack
-        print(f"Y")
+        if self.critical_chance == 1 :
+            enemy.enemy_health -= (self.attack * self.critical_damage_multiplier )
+            print(f"You did critical damage to {enemy.enemy_type} its health is now {enemy.enemy_health}")
+        else :
+            enemy.enemy_health -= self.attack
+            print(f"You did damage to {enemy.enemy_type} its health is now {enemy.enemy_health}")
     def get_attacked(self) :
         self.health -= enemy.enemy_attack
         print(f"You got hit by {enemy.enemy_type} for -{enemy.enemy_attack} now your health is {self.health}")
