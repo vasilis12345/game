c309165f7d4c7e6b199936273ba076427d0b323d
vasilis <009vasana@gmail.com>
2025-08-30 06:23:11 +0300

    new stuff added and fixed

--- fighting.py
+++ fighting.py
@@ -4,9 +4,13 @@ from hero import Hero,  gain_item
 hero = Hero()
 enemy = Enemy()
 
+RED = '\033[91m'
+GREEN = '\033[92m'
+BLUE = '\033[94m'
+RESET = '\033[0m'
 
 while not hero.dead:
-    character_input =input("Will you attack or heal??")
+    character_input =input(f"Will you {RED}attack{RESET} or {GREEN}heal{RESET}??")
     if character_input == "attack" :
         hero.do_attack()
         if enemy.enemy_health <= 0 :
