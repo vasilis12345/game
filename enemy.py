119edb96cdb7927d430bdeda3c5b689eb515d459
vasilis <009vasana@gmail.com>
2025-09-22 21:13:44 +0300

    new stuff added and fixed some other

--- enemy.py
+++ enemy.py
@@ -1,6 +1,8 @@
 import json
 import random
 
+from dulwich.objects import sorted_tree_items
+
 with open("enemy_config.json") as file:
     data = json.load(file)
 
@@ -27,4 +29,5 @@ class Enemy:
         self.enemy_xp = self.random_spawn["xp_held"]
         print(f"a {self.enemy_type} just spawned")
 
-
+sprite_idle  =  "                _                       __\n              /   \\                  /      \\\n             '      \\              /          \\\n            |       |Oo          o|            |\n            `    \\  |OOOo......oOO|   /        |\n             `    \\\\OOOOOOOOOOOOOOO\\//        /\n               \\ _o\\OOOOOOOOOOOOOOOO//. ___ /\n           ______OOOOOOOOOOOOOOOOOOOOOOOo.___\n            --- OO'* `OOOOOOOOOO'*  `OOOOO--\n                OO.   OOOOOOOOO'    .OOOOO o\n                `OOOooOOOOOOOOOooooOOOOOO'OOOo\n              .OO \"OOOOOOOOOOOOOOOOOOOO\"OOOOOOOo\n          __ OOOOOO`OOOOOOOOOOOOOOOO\"OOOOOOOOOOOOo\n         ___OOOOOOOO_\"OOOOOOOOOOO\"_OOOOOOOOOOOOOOOO\n           OOOOO^OOOO0`(____)/\"OOOOOOOOOOOOO^OOOOOO\n           OOOOO OO000/000000\\000000OOOOOOOO OOOOOO\n           OOOOO O0000000000000000 ppppoooooOOOOOO\n           `OOOOO 0000000000000000 QQQQ \"OOOOOOO\"\n            o\"OOOO 000000000000000oooooOOoooooooO'\n            OOo\"OOOO.00000000000000000000OOOOOOOO'\n           OOOOOO QQQQ 0000000000000000000OOOOOOO\n          OOOOOO00eeee00000000000000000000OOOOOOOO.\n         OOOOOOOO000000000000000000000000OOOOOOOOOO\n         OOOOOOOOO00000000000000000000000OOOOOOOOOO\n         `OOOOOOOOO000000000000000000000OOOOOOOOOOO\n           \"OOOOOOOO0000000000000000000OOOOOOOOOOO'\n             \"OOOOOOO00000000000000000OOOOOOOOOO\"\n  .ooooOOOOOOOo\"OOOOOOO000000000000OOOOOOOOOOO\"\n.OOO\"\"\"\"\"\"\"\"\"\".oOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\nOOO         QQQQO\"'                     `\"QQQQ\nOOO\n`OOo.\n  `\"OOOOOOOOOOOOoooooooo.\n\n"
+print(sprite_idle)
